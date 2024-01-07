33bd4630aa5a3a981369b101d0eec3dd434ef537
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2024-01-07 15:35:04 +0000

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "php-app-rds.cd22cmyu0ea0.us-east-2.rds.amazonaws.com";
+$username = "PHILIP";
+$password = "Philip123";
+$dbname = "db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
