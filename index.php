56f0c7c3f3341c699bbfb1678fbffe2001253346
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2024-01-07 15:43:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Simple Form</title>
-</head>
-<body>
-    <form method="post" action="submit.php">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name"><br><br>
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email"><br><br>
-        
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
+<?php
+
+echo '<h1>this is comming from codepipeline</h>';
